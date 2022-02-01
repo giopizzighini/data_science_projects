e1efa1cb5b9ffb296f997ca1cc7e991b5c6ccf7c
giopizzighini <91754869+giopizzighini@users.noreply.github.com>
2022-01-31 21:49:11 -0300

    Add files via upload
    Calculadora simples a partir de um exercício da internet! o/

---
+++ calculadora_gio.py
@@ -0,0 +1,31 @@
+print("**************** CALCULADORA PYTHON ********************")
+print("")
+print("Escolha uma opção: \n1 - Soma \n2 - Subtração \n3 - Divisão \n4 - Multiplicação")
+x = int(input("\nDigite: "))
+a = int(input("Digite o primeiro numero: "))
+b = int(input("Digite o segundo numero: "))
+
+def soma(a,b):
+    return a+b
+def sub(a,b):
+    return(a-b)
+def div(a,b):
+    return (a/b)
+def mult(a,b):
+    return (a*b)
+
+if x == 1:
+    print("\nO resultado é", soma(a,b))
+elif x == 2:
+    print("\nO resultado é", sub(a,b))
+elif x == 3:
+    print("\nO resultado é", div(a,b))
+elif x == 4:
+    print("\nO resultado é", mult(a,b))
+else:
+    print("Numero errado! Digite um número entre 1 e 4!")
+
+
+
+
+
